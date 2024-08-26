e2c5b2d3a8405176e0a28c31981e00de8edaf3c0
Harizz_TheGreat <harizadebayo8@gmail.com>
2024-08-26 14:58:56 -0700

    add an error page if refreshed and offline

--- src/Components/Body/NetworkTest.jsx
+++ src/Components/Body/NetworkTest.jsx
@@ -6,15 +6,25 @@ const Networktest = () => {
   const [isUserOnline, setIsUserOnline] = useState(navigator.onLine);
 
   useEffect(() => {
-    const Online = () => setIsUserOnline(true);
+    const Online = () => {
+      setIsUserOnline(true);
+      localStorage.setItem()
+    };
     const Offline = () => setIsUserOnline(false);
 
     window.addEventListener('online', Online);
     window.addEventListener('offline', Offline);
 
+    const hasRefreshed = window.addEventListener('beforeunload', Offline);
+
+    if(!isUserOnline && hasRefreshed ){
+      window.addEventListener('beforeunload', Offline);
+    } 
+
     return () => {
       window.removeEventListener('online', Online);
       window.removeEventListener('offline', Offline);
+      // window.addEventListener('beforeunload', Offline);
     };
   }, []);
 
